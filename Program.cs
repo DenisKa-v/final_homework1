76480e5c905c0ccbf87f73a9b9d4d6cc4c7a23af
DenisKa <pochta_dk@mail.ru>
2022-12-25 21:15:54 +0300

    Add method CreatInputArray in program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿string [] CreatInputArray (int size)
+{
+    string [] array = new string [size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write ($"Input element ({size-i} attempts left): ");
+        string element = Console.ReadLine();
+        array [i] = element;
+    }
+    return array;
+}
+
+Console.Write ("Нow many elements of array you enter: ");
+int el = Convert.ToInt32 (Console.ReadLine());
+
+string [] inputArray = CreatInputArray (el);
